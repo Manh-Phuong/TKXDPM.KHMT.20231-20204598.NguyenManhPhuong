63f906d3fe554f933a5ea386da9882161f123b2a
Manh-Phuong <phuong.nm23112002@gmail.com>
2023-12-16 22:38:06 +0700

    #44 - fix vnpay generateUrl

--- Programming/src/subsystem/interbank/VNPayController.java
+++ Programming/src/subsystem/interbank/VNPayController.java
@@ -13,65 +13,67 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class VNPayController {
-//    public static void main(String[] args) {
-//        try {
-//            VNPay.doPost();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    public static void doPost() throws ServletException, IOException {
-
+     public String generateRequest(Invoice invoice) throws UnsupportedEncodingException {
         String vnp_Version = "2.1.0";
         String vnp_Command = "pay";
-        String orderType = "other";
-//        long amount = Integer.parseInt(req.getParameter("amount"))*100;
-        long amount = 10000000;
-//        String bankCode = req.getParameter("bankCode");
+        String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+        String vnp_HashSecret =  "IVSETTETGUADTCASHCXYRLLJBCGAFMKI";
+        int amount = invoice.getAmount();
 
-        String vnp_TxnRef = Config.getRandomNumber(8);
-//        String vnp_IpAddr = Config.getIpAddress(req);
-
-        String vnp_TmnCode = Config.vnp_TmnCode;
-
-        Map<String, String> vnp_Params = new HashMap<>();
+        Map vnp_Params = new HashMap<>();
         vnp_Params.put("vnp_Version", vnp_Version);
         vnp_Params.put("vnp_Command", vnp_Command);
-        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_TmnCode", "XCWH8S3Q");
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
-//        vnp_Params.put("vnp_BankCode", "NCB");
-        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        String bank_code = "NCB";
+        if (bank_code != null && !bank_code.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bank_code);
+        }
+        vnp_Params.put("vnp_TxnRef", "1");
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang");
+        vnp_Params.put("vnp_OrderType", "20");
         vnp_Params.put("vnp_Locale", "vn");
 
-//        if (bankCode != null && !bankCode.isEmpty()) {
-//            vnp_Params.put("vnp_BankCode", bankCode);
-//        }
-
-//        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-//        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
-//        vnp_Params.put("vnp_OrderType", orderType);
-
-//        String locate = req.getParameter("language");
-//        if (locate != null && !locate.isEmpty()) {
-//            vnp_Params.put("vnp_Locale", locate);
-//        } else {
-//            vnp_Params.put("vnp_Locale", "vn");
-//        }
-//        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
-//        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+        vnp_Params.put("vnp_ReturnUrl", "localhost:5000");
+        vnp_Params.put("vnp_IpAddr", "12212121");
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
         cld.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(cld.getTime());
+        //Add Params of 2.1.0 Version
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+        //Billing
+        vnp_Params.put("vnp_Bill_Mobile", invoice.getOrder().getDeliveryInfo().get("phone"));
+        vnp_Params.put("vnp_Bill_Email", "");
 
+        String fullName = (String) invoice.getOrder().getDeliveryInfo().get("name");
+        if (fullName != null && !fullName.isEmpty()) {
+            int idx = fullName.indexOf(' ');
+            String firstName = fullName.substring(0, idx);
+            String lastName = fullName.substring(fullName.lastIndexOf(' ') + 1);
+            vnp_Params.put("vnp_Bill_FirstName", firstName);
+            vnp_Params.put("vnp_Bill_LastName", lastName);
+        }
+
+        vnp_Params.put("vnp_Bill_Address", invoice.getOrder().getDeliveryInfo().get("address"));
+        vnp_Params.put("vnp_Bill_City", invoice.getOrder().getDeliveryInfo().get("province"));
+        vnp_Params.put("vnp_Bill_Country", "Việt Nam");
+        vnp_Params.put("vnp_Bill_State", "1");
+        // Invoice
+        vnp_Params.put("vnp_Inv_Phone", "1");
+        vnp_Params.put("vnp_Inv_Email", "1");
+        vnp_Params.put("vnp_Inv_Customer", "1");
+        vnp_Params.put("vnp_Inv_Address", "1");
+        vnp_Params.put("vnp_Inv_Company", "1");
+        vnp_Params.put("vnp_Inv_Taxcode", "1");
+        vnp_Params.put("vnp_Inv_Type", "1");
+        //Build data to hash and querystring
         List fieldNames = new ArrayList(vnp_Params.keySet());
         Collections.sort(fieldNames);
         StringBuilder hashData = new StringBuilder();
@@ -96,15 +98,33 @@ public class VNPayController {
             }
         }
         String queryUrl = query.toString();
-        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+        String vnp_SecureHash = hmacSHA512(vnp_HashSecret, hashData.toString());
         queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
-//        com.google.gson.JsonObject job = new JsonObject();
-//        job.addProperty("code", "00");
-//        job.addProperty("message", "success");
-//        job.addProperty("data", paymentUrl);
-//        Gson gson = new Gson();
-//        resp.getWriter().write(gson.toJson(job));
-        System.out.println(paymentUrl);
+        String paymentUrl = vnp_Url + "?" + queryUrl;
+        return paymentUrl;
     }
+
+    public static String hmacSHA512(final String key, final String data) {
+        try {
+
+            if (key == null || data == null) {
+                throw new NullPointerException();
+            }
+            final Mac hmac512 = Mac.getInstance("HmacSHA512");
+            byte[] hmacKeyBytes = key.getBytes();
+            final SecretKeySpec secretKey = new SecretKeySpec(hmacKeyBytes, "HmacSHA512");
+            hmac512.init(secretKey);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            byte[] result = hmac512.doFinal(dataBytes);
+            StringBuilder sb = new StringBuilder(2 * result.length);
+            for (byte b : result) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+    
 }
